4e4ea9cc707591386bf64621147fbd51b30e1bb3
Danny Eiden <deiden26@gmail.com>
2015-02-05 01:02:08 -0600

    Added more sensible camera tracking that follows penguin specifically.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,13 +5,22 @@ public class CameraController : MonoBehaviour {
 
 	public PenguinController penguin;
 
+	private Vector3 nextPosition;
+	private Transform penguinTransform;
+
 	// Use this for initialization
 	void Start () {
-		rigidbody2D.velocity = new Vector3 (1, 0, 0) * penguin.forwardVelocity;
+		//Cache reference to penguin transform
+		GameObject penguin = GameObject.Find ("penguin");
+		penguinTransform = penguin.transform;
+		//Initialize next position
+		nextPosition = camera.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Track penguins motion in the x direction
+		nextPosition.x = penguinTransform.position.x;
+		camera.transform.position = nextPosition;
 	}
 }
