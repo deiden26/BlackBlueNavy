5530a412a1bd9a259aa9c2220350e9b7c3d40190
evtressler <elizabethtressler2015@u.northwestern.edu>
2015-02-18 12:08:57 -0600

    uploading my changes without breaking everything this time hopefully

---
+++ Assets/Scripts/startGame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void LoadLevel() {
+		Application.LoadLevel ("runScene");
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseDown () {
+		LoadLevel ();
+	}
+
+}
