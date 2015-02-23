868c95164a2954f6102bb33050f3e7898f88e600
Danny Eiden <deiden26@gmail.com>
2015-02-23 01:29:49 -0600

    Add getting hurt animation to provide feedback that spikes are evil

---
+++ Assets/Scripts/runSceneUIManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class runSceneUIManager : MonoBehaviour {
+
+	/*~~~~~~ private variables ~~~~~~*/
+	float hurtOverlayAlpha;
+	Slider healthSlider;
+	Image hurtOverlay;
+
+	/*~~~~~~ unity functions ~~~~~~*/
+
+	void Start() {
+		healthSlider = this.transform.Find ("healthBar").GetComponent<Slider> ();
+		hurtOverlay = this.transform.Find ("hurtOverlay").GetComponent<Image> ();
+		hurtOverlayAlpha = 0;
+	}
+
+	void Update() {
+		if (hurtOverlayAlpha != 0) {
+			Color newColor = hurtOverlay.color;
+			newColor.a = hurtOverlayAlpha;
+			hurtOverlay.color = newColor;
+			hurtOverlayAlpha = hurtOverlayAlpha - 0.02f;
+		}
+	}
+
+	void OnEnable()
+	{
+		PenguinController.onHealthChange += changeHealth;
+	}
+	
+	
+	void OnDisable()
+	{
+		PenguinController.onHealthChange -= changeHealth;
+	}
+
+	/*~~~~~~ private functions ~~~~~~*/
+
+	void changeHealth(float newHealth) {
+		if (newHealth < healthSlider.value)
+			hurtOverlayAlpha = 1;
+		healthSlider.value = newHealth;
+	}
+
+}
