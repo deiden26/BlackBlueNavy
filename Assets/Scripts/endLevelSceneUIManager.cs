6c65eb1982bba296452fd170c461cbead3a385bb
Danny Eiden <deiden26@gmail.com>
2015-03-06 17:50:14 -0600

    Add local high score saving system

--- Assets/Scripts/endLevelSceneUIManager.cs
+++ Assets/Scripts/endLevelSceneUIManager.cs
@@ -11,10 +11,37 @@ public class endLevelSceneUIManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		//Get new user score
 		int score = PlayerPrefs.GetInt ("score");
+
+		//Get user score text element
 		Text scoreText = this.transform.Find ("textHolder").transform.Find ("scoreHolder").transform.Find ("score").GetComponent<Text> ();
 
+		//Get high score text element
+		Text highScoresText = this.transform.Find ("textHolder").transform.Find ("highScoresHolder").transform.Find ("highScores").GetComponent<Text> ();
+
+		//Fill user score text element
 		string scoreString = string.Format ("Score: {0}", score);
 		scoreText.text = scoreString;
+
+		//Get all high scores
+		int[] highScores = new int[10];
+
+
+		bool scoreNotSet = true;
+		string highScoresString = "";
+		for (int i=0; i < 10; i++) {
+			string highScoreLookupString = string.Concat("highScore", i);
+			highScores [i] = PlayerPrefs.GetInt (highScoreLookupString);
+			if (score > highScores[i] && scoreNotSet) {
+				PlayerPrefs.SetInt(highScoreLookupString, score);
+				highScores[i] = score;
+				scoreNotSet = false;
+			}
+			highScoresString += string.Format("{0}\n", highScores[i]);
+		}
+
+		highScoresText.text = highScoresString;
+
 	}
 }
